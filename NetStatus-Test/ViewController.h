060ee3a6d57623e248e67f126bcc460cefe59154
ChengLuffy <chengluffy@hotmail.com>
2016-02-25 16:08:59 +0800

    Initial Commit

---
+++ NetStatus-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetStatus-Test
+//
+//  Created by 成璐飞 on 16/2/25.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NetStatus-Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NetStatus-Test
+//
+//  Created by 成璐飞 on 16/2/25.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
