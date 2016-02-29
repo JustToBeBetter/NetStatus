21f335bbfd3b26a797cabce6e8392f9f6691233b
JustToBeBetter <li@li.local>
2016-02-29 11:35:27 +0800

    netStatus
    网络状态检测

--- NetStatus-Test/ViewController.m
+++ NetStatus-Test/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "ChechNetStatus.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -17,11 +19,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)checkNetStatus:(id)sender {
+    
+    NSInteger status = [ChechNetStatus checkDreviceNetStatus];
+    switch (status) {
+        case 0:
+            self.label.text = @"没有网络";
+            break;
+        
+        case 1:
+            self.label.text = @"wifi";
+            break;
+            
+        case 2:
+            self.label.text = @"数据";
+            break;
+            
+        default:
+            break;
+    }
+    
 }
 
 @end
